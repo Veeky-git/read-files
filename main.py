213c50d370520db4b62e8bc71e4789a216f2e215
Victoria Tabugbo <victoriachi39@gmail.com>
2022-05-26 22:16:15 +0100

    edited story.txt file

--- main.py
+++ main.py
@@ -3,14 +3,25 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
+import string 
+
 def read_file_content(filename):
-    # [assignment] Add your code here 
-    
-    return "Hello World"
+    with open(filename) as f: 
+        content = f.read()
+    return content
+print(read_file_content("story.txt"))
 
 
 def count_words():
-    text = read_file_content("./story.txt")
-    # [assignment] Add your code here
-
-    return {"as": 10, "would": 20}
+    text = read_file_content("story.txt")
+    d = {}
+    words = text.split()
+    table = str.maketrans("", "", string.punctuation)
+    stripped = {w.translate(table) for w in words}
+    for words in stripped:
+        if words in d: 
+            d[words] = [words] + 1
+        else:
+            d[words] = 1
+    return d
+print(count_words())
